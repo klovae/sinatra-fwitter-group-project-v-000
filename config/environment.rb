8c062bf3fa8d9b92a0291a797f9af2a221855b02
Elyse Klova <elyse.klova@gmail.com>
2016-12-14 22:17:26 -0600

    modified environment file to configure production

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,22 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+end
+
+configure :production do
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => "postgres"
+   :database => "db/production.db"
+ )
+end
 
 require 'rack-flash'
 require_all 'app'
